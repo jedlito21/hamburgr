06a692dcf50914bcec63750a1bf923986e9e93f2
tomas <jedlito21@sps-prosek.cz>
2023-03-07 21:40:11 +0100

    Dodělat grafiku

--- main.py
+++ main.py
@@ -8,67 +8,71 @@ okno.title("Hamburgr konfigurace")
 
 # nazev formu
 nazev_form = Label(okno, text="Konfigurátor burgeru",width=20,font=("bold", 40))
-nazev_form.place(x=0,y=53)
-
-
+nazev_form.pack()
 # nazev burgeru
 nazev_burgeru = Label(okno, text="Nazev burgeru",width=20,font=("bold", 16))
-nazev_burgeru.place(x=50,y=130)
+nazev_burgeru.pack()
 
 nazev_burgeru_vstup = Entry(okno)
-nazev_burgeru_vstup.place(x=250,y=135)
+nazev_burgeru_vstup.pack()
 
 # propečenost masa burgeru
 propecenost = Label(okno, text="Vyberte propečenost masa",width=20,font=("bold", 16))
-propecenost.place(x=95,y=175)
+propecenost.pack()
 
 propecenost_vyber = IntVar() # propecenost_vyber je variable ke který přiřazuju radiobuttony
-Radiobutton(okno, text="Well-Done",padx = 5, variable=propecenost_vyber, value=1).place(x=100,y=200)
-Radiobutton(okno, text="Medium-Well",padx = 5, variable=propecenost_vyber, value=2).place(x=100,y=220)
-Radiobutton(okno, text="Medium",padx = 5, variable=propecenost_vyber, value=3).place(x=100,y=240)
-Radiobutton(okno, text="Rare",padx = 5, variable=propecenost_vyber, value=4).place(x=100,y=260)
-Radiobutton(okno, text="Medium-Rare",padx = 5, variable=propecenost_vyber, value=5).place(x=100,y=280)
-Radiobutton(okno, text="Blue-Rare",padx = 5, variable=propecenost_vyber, value=6).place(x=100,y=300)
+Radiobutton(okno, text="Well-Done",padx = 5, variable=propecenost_vyber, value=1).pack()
+Radiobutton(okno, text="Medium-Well",padx = 5, variable=propecenost_vyber, value=2).pack()
+Radiobutton(okno, text="Medium",padx = 5, variable=propecenost_vyber, value=3).pack()
+Radiobutton(okno, text="Rare",padx = 5, variable=propecenost_vyber, value=4).pack()
+Radiobutton(okno, text="Medium-Rare",padx = 5, variable=propecenost_vyber, value=5).pack()
+Radiobutton(okno, text="Blue-Rare",padx = 5, variable=propecenost_vyber, value=6).pack()
 
 # druhy_zemli
 druhy_zemli = ["Klasická", "Sezamová", "Bramborová", "Kaiserka", "Ciabatta"]
 clicked = StringVar()
 clicked.set("Klasická")
 drop = OptionMenu( okno , clicked , *druhy_zemli )
-drop.pack()
 
 zemle = Label(okno, text="Vyberte druh žemle", width=20, font=("bold", 16))
-zemle.place(x=60,y=320)
-drop.place(x=100,y=350)
+zemle.pack()
+drop.pack()
 
 
 # zelenina
 zelenina = Label(okno, text="Vyberte si zeleninu", width =20, font=("bold", 16))
-zelenina.place(x=60,y=380)
+zelenina.pack()
 
 rajce_vyber = IntVar()
 salat_vyber = IntVar()
 okurka_vyber = IntVar()
 cibule_vyber = IntVar()
 
-Checkbutton(okno, text="rajče",variable=rajce_vyber).place(x=100,y=410)
-Checkbutton(okno, text="salát",variable=salat_vyber).place(x=100,y=430)
-Checkbutton(okno, text="okurka",variable=okurka_vyber).place(x=100,y=450)
-Checkbutton(okno, text="cibule",variable=cibule_vyber).place(x=100,y=470)
+Checkbutton(okno, text="rajče",variable=rajce_vyber).pack()
+Checkbutton(okno, text="salát",variable=salat_vyber).pack()
+Checkbutton(okno, text="okurka",variable=okurka_vyber).pack()
+Checkbutton(okno, text="cibule",variable=cibule_vyber).pack()
 
 # omacky
 omacky = ["Kečup", "Majonéza", "BBQ", "Texaský dresing"]
 vyber_omacky = StringVar()
 vyber_omacky.set("Kečup")
 drop_omacky = OptionMenu( okno , vyber_omacky , *omacky )
-drop_omacky.pack()
 
 omacky_text = Label(okno, text="Vyberte omačku", width=20, font=("bold", 16))
-omacky_text.place(x=60,y=500)
-drop_omacky.place(x=100,y=530)
+omacky_text.pack()
+drop_omacky.pack()
 
 #submit
-Button(okno, text='Submit',width=20,bg='green',fg='white').place(x=180,y=570)
+def exit():
+    okno.destroy()
+def submit():
+    for widgets in okno.winfo_children():
+        widgets.destroy()
+
+    Button(okno, text='Zavřít', width=20, bg='red', fg='white', command=exit).pack()
+
+Button(okno, text='Submit',width=20,bg='green',fg='white', command=submit).pack()
 
 okno.mainloop()
 
